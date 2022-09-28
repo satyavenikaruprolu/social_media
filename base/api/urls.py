8c78832e2930fc25843a152caa28248c28c9a6a7
satyavenikaruprolu <satyakaruprolu129@gmail.com>
2022-09-28 12:54:13 +0530

    developed study bud

---
+++ base/api/urls.py
@@ -0,0 +1,9 @@
+import imp
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.getRoute),
+    path('rooms/',views.getRooms),
+    path('rooms/<str:pk>/',views.getRoom),
+]
